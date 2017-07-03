4a8fed560f2ffd4a60076d08271484c684357fef
Daniel Schwab <daniel.schwab@infonova.com>
2017-07-03 11:22:51 +0200

    animate 2

--- src/app/flight-booking/flight-search/flight-card.component.ts
+++ src/app/flight-booking/flight-search/flight-card.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, EventEmitter, Output, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Flight } from '../../entities/flight';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
-  selector: 'flight-card',
-  templateUrl: './flight-card.component.html',
+    selector: 'flight-card',
+    templateUrl: './flight-card.component.html',
     animations: [
         trigger('select', [
             state('0', style({
@@ -19,48 +19,75 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
             state('*', style({
                 'background-color': 'lightgrey'
             })),
-            transition('0 => 1', animate('1s 200ms cubic-bezier(0.19,0.88,1,0.52)')),
+            transition('1 => 0', animate('1s 200ms cubic-bezier(0.19,0.88,1,0.52)')),
+            transition('0 => 1', [
+                style({
+                    'opacity': 0
+                }),
+                group([
+                    animate(500, keyframes([
+                        style({transform: 'translate3d(0,-30px,0)', offset: 0.2}),
+                        style({transform: 'translate3d(0,50px,0)', offset: 0.4}),
+                        style({transform: 'translate3d(0,-40px,0)', offset: 0.9}),
+                        style({transform: 'translate3d(0,0,0)', offset: 1})
+                    ])),
+                    animate(300, style({
+                        'opacity': 1
+                    }))
+                ]),
+                animate(500)
+            ]),
             transition('* => *', animate(500))
         ])
     ]
 })
-export class FlightCardComponent implements OnInit, OnChanges{
+export class FlightCardComponent implements OnInit, OnChanges {
 
 
-  @Input() item: Flight;
-  @Input() selected: boolean;
-  @Output() selectedChange = new EventEmitter<boolean>();
+    @Input() item: Flight;
+    @Input() selected: boolean;
+    @Output() selectedChange = new EventEmitter<boolean>();
 
-  select() {
-    this.selected = true;
-    this.selectedChange.next(this.selected);
-  }
+    select() {
+        this.selected = true;
+        this.selectedChange.next(this.selected);
+    }
 
-  deselect() {
-    this.selected = false;
-    this.selectedChange.next(this.selected);
-  }
+    deselect() {
+        this.selected = false;
+        this.selectedChange.next(this.selected);
+    }
 
-  constructor() {
-    // console.debug('ctor', this.selected, this.item);
-  }
+    disabled = false;
 
-  ngOnInit(): void {
-    // console.debug('init', this.selected, this.item);
+    start() {
+        this.disabled = true;
+    }
 
-    // Böse Aktion!
-    //this.selectedChange.next(true);
-  }
+    done() {
+        this.disabled = false;
+    }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    // console.debug('changes', this.selected, this.item);
-    // if (changes['selected']) {
-    //   console.debug('selected changed');
-    // }
-    // if (changes['item']) {
-    //   console.debug('item changed');
-    // }
+    constructor() {
+        // console.debug('ctor', this.selected, this.item);
+    }
 
-  }
+    ngOnInit(): void {
+        // console.debug('init', this.selected, this.item);
+
+        // Böse Aktion!
+        //this.selectedChange.next(true);
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        // console.debug('changes', this.selected, this.item);
+        // if (changes['selected']) {
+        //   console.debug('selected changed');
+        // }
+        // if (changes['item']) {
+        //   console.debug('item changed');
+        // }
+
+    }
 
 }
